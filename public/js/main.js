bd3c30a2381185e6e3da3b9e149afa7ae5d939b3
Austin Ellsworth <austinellsworth@gmail.com>
2017-11-18 00:45:56 -0500

    tidy up

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,9 @@
+MUSIC.init()
+MAP.init()
 
-// ================================================================================
-// ==================== add event listeners to music control buttons ====================
-// ================================================================================
-
+// Event listeners for music controls
 document.getElementById('back').addEventListener('click', MUSIC.songBack)
 document.getElementById('play').addEventListener('click', MUSIC.playPause)
 document.getElementById('forward').addEventListener('click', MUSIC.songForward)
 document.getElementById('shuffle').addEventListener('click', MUSIC.songShuffle)
 document.getElementById('playlists').addEventListener('change', function () { MUSIC.changePlaylist(this.value) })
-
-MUSIC.init()
-MAP.init()
